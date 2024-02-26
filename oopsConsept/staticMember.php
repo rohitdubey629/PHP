7c7b9cdb31c0ae1130745afe285f39a2147968a8
rohitdubey629 <dubeyrohit629@gmail.com>
2024-02-26 19:11:44 +0530

    implement static late bainding

--- oopsConsept/staticMember.php
+++ oopsConsept/staticMember.php
@@ -2,37 +2,17 @@
 
 class base1{
 
-// static run vithout create object 
-    public static $name = "Rohit dubey";
+    protected static $name = "Rohit";
 
     public static function show(){
-        echo self::$name. "<br>";
+        echo static::$name. "<br>";
+        echo self::$name;
     }
-
-    // public function __construct()
-    // {
-    //     self::show();
-    // }
-    // public function __construct($n)
-    // {
-    //     self::$name = $n;
-    // }
 }
 
 class drive1 extends base1{
-    public static function show(){
-        echo base1::$name;
-    }
+protected static $name = "Dubey";
 }
-
-// echo base1::$name;
-// base1::show();
-
-
-// $p1 = new base1();
-// $p1->show();
-// $p1 = new base1("shubham");
-// $p1->show();
 
 $p1 = new drive1();
 $p1->show();
